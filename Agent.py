931547892ca64aa1eb788d47ec995b8be9a8b4f5
Keshavshetty707 <mkeshav9241217275@gmail.com>
2024-09-18 11:28:27 +0530

    major flaw was solved  now we are getting the output

--- Agent.py
+++ Agent.py
@@ -1,15 +1,15 @@
 #this file excute the resprective function based on user query
+import task1
 from config import key
 import requests #web libraries
-import task1
 
 def run_conversation(user_message):
-     messages = [] # list which all messages
+    messages = [] # list which all messages
 
     system_message = """You are an AI Chat bot, that can do everything using function call. When you are asked to do something, use the function call you have available and then respond with message"""#first instruction
    
     message = { "role": "user", 
-               "parts": [{"text": system_message+"\n"+user_message}] 
+                "parts": [{"text": system_message+"\n"+user_message}] 
                }
     
     messages.append(message)
@@ -23,9 +23,9 @@ def run_conversation(user_message):
                 ]   
             }
     url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="+key
-    response = requests.post(url, json=data)
+    response = requests.post(url, json = data)
     
-    if response.status_code !=200:
+    if response.status_code != 200:
         print(response.text)
 
     t1 = response.json()
@@ -37,5 +37,7 @@ def run_conversation(user_message):
     #t2= t1.get("candidates")[0].get("content").get("parts")[0].get("text")
     #print(t2)
     print("now we are getting", t1)
-    if __name__ == " __main__":
-    
+
+if __name__ == "__main__":
+    user_messages = "find the temperature of Delhi"
+    run_conversation(user_messages)
